0a842f00a28eb965047b8b3112f7c5236e95bbd3
Ryan Vazquez <ryanmvazquez@gmail.com>
2023-07-19 14:53:58 -0400

    Update filter.ts

--- src/arrays/filter.ts
+++ src/arrays/filter.ts
@@ -4,6 +4,9 @@
 // The filter function should return the new array with only the elements that pass
 // the predicate function
 
+// const evens = filter([1,2,3], n => n % 2 === 0); // [2]
+// const odds = filter([2,4,6], n => n % 2 !== 0);  // []
+
 // **REMOVE THIS LINE**
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function filter<T>(array: Array<T>, callback: (el: T) => boolean): Array<T> {
