543c83f091db3b89b87767c1a8a25d38b046aabe
Ryan Vazquez <ryanmvazquez@gmail.com>
2023-07-19 14:54:37 -0400

    Update forEach.ts

--- src/arrays/forEach.ts
+++ src/arrays/forEach.ts
@@ -2,6 +2,8 @@
 // and a callback as parameters. The callback will be invoked
 // for each element in the array. This function should not return anything.
 
+// forEach(["hello", "world"], word => console.log(word));
+
 // **REMOVE THIS LINE**
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function forEach<T>(array: Array<T>, callback: (element: T) => void): void { }
