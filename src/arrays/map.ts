51676dc32d1f7ffb8053d180dbe927204743f71a
Ryan Vazquez <ryanmvazquez@gmail.com>
2023-07-19 14:56:40 -0400

    Update map.ts

--- src/arrays/map.ts
+++ src/arrays/map.ts
@@ -2,6 +2,8 @@
 // Each element of the array will be passed to the callback, which when invoked will return a new element.
 // The function should return a new array of the transformed elements.
 
+// const capitalized = map(["hello", "world"], word => word.toUpperCase()); // ["HELLO", "WORLD"]
+
 // **REMOVE THIS LINE**
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function map<T>(array: Array<T>, callback: (element: T) => T): Array<T> {
