3699ab9f8d33b641593eae6805015ff6de7ad95a
Ryan Vazquez <ryanvazquez@github.com>
2023-07-19 12:53:43 -0400

    suppress esmodule interop warning

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  // https://github.com/kulshekhar/ts-jest/issues/1173#issuecomment-1404142039
+  transform: {
+    '^.+\\.(ts|tsx)?$': ['ts-jest', {diagnostics: {ignoreCodes: ['TS151001']}}],
+    "^.+\\.(js|jsx)$": "babel-jest",
+  }
 };
