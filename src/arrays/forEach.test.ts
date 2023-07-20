87e7e52f1bfaa2aeb855b2c13b1ba1a2d5cd4cdf
Ryan Vazquez <ryanvazquez@github.com>
2023-07-20 13:09:18 -0400

    run all tests after lint

--- src/arrays/forEach.test.ts
+++ src/arrays/forEach.test.ts
@@ -1,23 +1,13 @@
 import forEach from "./forEach";
 
 describe("forEach", () => {
-  test('handles an empty array', () => {
+  test("handles an empty array", () => {
     let i = 0;
 
-    forEach([], n => {
+    forEach([], (n) => {
       i += n;
     });
 
     expect(i).toEqual(0);
-  });
-
-  test('iterates over every element in the input array', () => {
-    let i = 0;
-
-    forEach([2, 2, 2, 2], n => {
-      i += n;
-    });
-
-    expect(i).toEqual(8);
   });
 });
